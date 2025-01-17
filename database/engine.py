6d304509043b543c553d9bda89bb98fa6fea2b5e
someicon <elmeev.s.m@gmail.com>
2025-01-17 11:57:34 +0300

    docker + docker-compose.yaml

--- database/engine.py
+++ database/engine.py
@@ -1,5 +1,9 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from config.db_config import settings
 
 user = settings.DB_USER
@@ -9,6 +13,32 @@ port = settings.DB_PORT
 db = settings.DB_NAME
 
 
+# Проверка создана ли БД
+
+conn = psycopg2.connect(
+    dbname="postgres",
+    user=user,
+    password=password,
+    host=host,
+    port=port,
+)
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+cursor = conn.cursor()
+
+cursor.execute(f"SELECT 1 FROM pg_catalog.pg_database WHERE datname = '{db}'")
+exists = cursor.fetchone()
+
+if not exists:
+    # Создаём базу данных
+    cursor.execute(f"CREATE DATABASE {db}")
+    print(f"Database {db} created successfully.")
+else:
+    print(f"Database {db} already exists.")
+
+cursor.close()
+conn.close()
+
+# Подключение к бд
 DB_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
 
 # отключает ограничение на подключение только в одном потоке
